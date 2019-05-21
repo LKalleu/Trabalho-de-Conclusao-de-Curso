8e892f3e5329dabf9b4e0335a2400265921f0fff
LKalleu <kalleu157@gmail.com>
2019-05-21 18:09:48 -0300

    Detalhes

--- View/produtos.php
+++ View/produtos.php
@@ -3,7 +3,7 @@ ini_set("display_errors",1);
 ini_set("display_startup_erros",1);
 error_reporting(E_ALL);
 
-include 'config.php';
+include '../Model/config.php';
 
 //$registro=$coneccao->fetchAll(PDO::FETCH_OBG);
 
